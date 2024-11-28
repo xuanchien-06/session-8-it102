0d7f34e85848e29eb69bd2fb4c81f2c2fe88be0a
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2024-11-28 14:40:50 +0700

    session 8 bài 1

---
+++ session 8 bai 1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+//khai bao va gan gia tri cho mang bat ky
+//in ra tung phan tu trong mang tu cuoi ve dau
+int main(){
+	int arr[5]={1,2,3,4,5};
+	for(int i=5;i>0;i--){
+		printf("%3d",i);
+	}
+	return 0;
+}
